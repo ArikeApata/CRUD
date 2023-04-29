f05cfb3aab442b5c985d37ace0852c78c09f1e3f
freemile2ru <sofiyullahj@gmail.com>
2023-04-29 23:37:01 +0100

    frontend initial push

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
-const express = require('express')
-const fs = require("fs");
-
+const express = require("express");
+const path = require("path");
 
 const app = express();
 app.use(express.json());
 
-const userRouter = require('./routes/users')
+const userRouter = require("./routes/users");
 
-app.use("/users", userRouter)
+app.use("/users", userRouter);
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
 //console.log(users);
-const PORT = process.env.PORT || 4000
+const PORT = process.env.PORT || 4000;
 
-app.listen(PORT, () => console.log(`listening on ${PORT}`))
-
-
+app.listen(PORT, () => console.log(`listening on ${PORT}`));
---
+++ public/index.js
@@ -0,0 +1,3 @@
+fetch("http://localhost:4000/users/list");
+
+fetch("/users/list");
